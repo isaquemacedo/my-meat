03e862756d66c5a71d3bdb4ca596fbe0efa135db
Isaque Alves <isaque.mace@gmail.com>
2019-04-10 00:00:36 -0300

    Alterando o ShareModule para utiliza-lo com o ModuleWithProviders

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
 import { RatingComponent } from "./rating/rating.component";
+import { OrderService } from '../order/order.service';
+import { RestaurantServices } from '../restaurants/restaurants.service';
+import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
 
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent],
@@ -15,5 +18,10 @@ import { RatingComponent } from "./rating/rating.component";
 })
 
 export class SharedModule {
-
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SharedModule,
+            providers: [ShoppingCartService, RestaurantServices, OrderService]
+        }
+    }
 }
