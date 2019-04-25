8270b372166db1021fb8af303e92248cf1247c21
Isaque Alves <isaque.mace@gmail.com>
2019-04-25 00:09:50 -0300

    Animação no input de busca

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -3,19 +3,41 @@ import { Component, OnInit } from '@angular/core';
 import {Restaurant} from './restaurant/restaurant.model'
 import {RestaurantServices} from './restaurants.service'
 
+import { trigger, style, state, transition, animate } from '@angular/animations';
+
 @Component({
   selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+  templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        "max-height": "0px"
+      })),
+      state('visible', style({
+        opacity: 1,
+        "max-height": "70px",
+        "margin-top": "20px"
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
 
   restaurants: Restaurant[]
+
+  searchBarState = 'hidden'
 
   constructor(private restaurantServices: RestaurantServices) { }
 
   ngOnInit() {
     this.restaurantServices.restaurants()
     .subscribe(restaurants => this.restaurants = restaurants)
+  }
+
+  toggleSearch() {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden'
   }
 
 }
